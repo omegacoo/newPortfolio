88836d17573681f79e40febfeb17cc90c5c40a43
omegacoo <bsumser@yahoo.com>
2019-10-04 17:57:24 -0500

    basic contact screen set up

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ function contactClick(){                        // Binding function to contact b
 function backToLanding(){
     $('*').on('click', function(e){
         if(STATE.screen !== 'landing'){
-            if(!$(e.target).hasClass('js-content') && !$(e.target).hasClass('js-buttons')){
+            if(!$(e.target).hasClass('js-content-window') && !$(e.target).hasClass('js-buttons')){
                 STATE.screen = 'landing';
                 renderMainElement();
             };
@@ -37,27 +37,38 @@ function backToLanding(){
 function buildCurrentElement(source){           // Creating HTML based on STATE.screen
     if(STATE.screen === "landing"){
         return `
-        <button class="js-buttons" id="bio">Bio</button>
-        <button class="js-buttons" id="projects">Projects</button>
-        <button class="js-buttons" id="contact">Contact</button>
+        <button class="js-buttons js-content-window" id="bio">Bio</button>
+        <button class="js-buttons js-content-window" id="projects">Projects</button>
+        <button class="js-buttons js-content-window" id="contact">Contact</button>
         `
     }if(STATE.screen === "bio"){
         return `
-        <div class="js-content">
-            <h3>About Me</h3>
-            <p></p>
+        <div class="js-content js-content-window">
+            <h3 class="js-content-window">Bio</h3><br>
+            <p class="js-content-window">&emsp;I am an extremely passionate programmer. I have a BA in Philosophy and have always enjoyed the logical aspects of coding.</p><br>
+            <p class="js-content-window">&emsp;I have been in the Army for 17 years and am a Company Commander. I have great personal discipline, integrity, and the ability to work with people from all walks of life, without conflict.</p><br>
+            <p class="js-content-window">&emsp;It is my dream to become a web developer, to make your company a better place, and help it develop a better product.</p><br>
+            <p class="js-content-window">&emsp;On my own time I try to keep up to date with current trends in the web development field as well as learn other languages and best practices.</p>
         </div>
         `
     }if(STATE.screen === "projects"){
         return `
-        <div class="js-content">
+        <div class="js-content js-content-window">
             
         </div>
         `
     }if(STATE.screen === "contact"){
         return `
-        <div class="js-content">
-            
+        <div class="js-content js-content-window">
+            <section class="heading js-content-window" role="heading">
+                <h3 class="js-content-window">Contact</h3>
+                <p class="js-content-window">Reach out to me in any of the following ways:</p>
+            </section>
+            <section role="links" class="links js-content-window">
+                <p class="js-content-window">Email: <a class="js-content-window" href="bsumser@yahoo.com">bsumser@yahoo.com</a></p>
+                <p class="js-content-window">GitHub: <a class="js-content-window" href="https://github.com/omegacoo">Omegacoo</a></p>
+                <p class="js-content-window">LinkedIn: <a class="js-content-window" href="https://www.linkedin.com/in/benjamin-sumser-863a3a182/">Ben Sumser</a></p>
+            </section>
         </div>
         `
     }
